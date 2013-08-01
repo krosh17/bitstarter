79f5dd50b0713e92c5958968fbd1130a61e56dc2
D Yu <krosh17@gmail.com>
2013-08-01 14:17:22 +0000

    Added index in web.js.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var text=fs.readFile("index.html",String)
+
+
 app.get('/', function(request, response) {
-  response.send('Welcome back Shevaldos!');
+  response.send('Welcome back Shevaldos!'+text);
 });
 
 var port = process.env.PORT || 5000;
