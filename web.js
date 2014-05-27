948a93e052bbdf0d5bc287b79ed653721e944e18
Dyu <yurasov17@gmail.com>
2014-05-27 22:43:18 +0400

    ne
    w

--- web.js
+++ web.js
@@ -1,16 +1,15 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
-
 var fs=require('fs');
+var htmlfile= "index.html";
 
-var buff= new Buffer(fs.readFileSync("index.html"),'utf-8');
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Welcome back Shevaldos!'+buff.toString());
+    var html = fs.readFileSync(htmlfile).toString();  
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
